40d3b7bfc49525c675573569b757ae5835f51f9f
william fevre <fevre.william@wanadoo.fr>
2023-06-19 16:09:45 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: "pools#index"
+  resources :pools, except: [:edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:update, :show, :edit, :destroy]
 end
