4daf7bfcbbe62800cad7044951dc905ab1a6df34
william fevre <fevre.william@wanadoo.fr>
2023-06-19 19:20:58 +0200

    add migration and views correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "----------- delete Pool -----------"
+Pool.destroy_all
+puts "----------- create Pool -----------"
+User.create(first_name: 'john', last_name: "pool")
+User.create(first_name: 'jack', last_name: "poolMaster")
+
+Pool.create(name: 'good pool', description: "vraiment une good pool", max_people: 5, price: "100$", address: "Paris")
+Pool.create(name: 'nice pool', description: "nice pool", max_people: 2, price: "200$", address: "London")
+puts "----------- OK -----------"
