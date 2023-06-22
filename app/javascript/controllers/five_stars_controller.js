2fb42f009259869196d893e707dd341a3f17c619
gunther tresor tchogmakwe yango <3106802632@qq.com>
2023-06-22 10:41:57 +0200

    show form uppate

--- app/javascript/controllers/five_stars_controller.js
+++ app/javascript/controllers/five_stars_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
 
 // Connects to data-controller="five-stars"
 export default class extends Controller {
   static targets = ["stars"]
   connect() {
     console.log(this.starsTarget)
+    flatpickr(this.element)
   }
 }
